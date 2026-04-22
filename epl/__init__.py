8845b5669404bfa75f54f09a9fe47062bf7b3c29
Abneesh Singh <singhabneesh250@gmail.com>
2026-04-22 17:29:34 +0530

    chore: bump version to v7.5.0 for EPL and v1.1.0 for VS Code extension

--- epl/__init__.py
+++ epl/__init__.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 from importlib import import_module
 from typing import TYPE_CHECKING, Any
 
-__version__ = "7.4.5"
+__version__ = "7.5.0"
 __author__  = "Abneesh Singh"
 __email__   = "singhabneesh250@gmail.com"
 __license__ = "MIT"
